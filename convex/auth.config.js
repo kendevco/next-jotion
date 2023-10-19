b80a6035772189056bc8d58297484dbb496147f5
Kenneth Courtney <kenneth.courtney@gmail.com>
2023-10-19 12:17:30 -0400

    Initial commit

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+
+// @ts-ignore
+export default {
+    providers: [
+      {
+        domain: "https://intent-humpback-54.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
